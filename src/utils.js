7fa470b21d89441240b2f492151632db9eb7c04b
DDeShon <36036284+DDeShon@users.noreply.github.com>
2024-11-21 22:08:03 -0600

    working on utils.js

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,8 @@
 export function displayDialogue(text, onDisplayEnd) {
   {
     const dialogueUI = document.getElementById("textbox-container");
+    const dialogue = document.getElementById("dialogue");
+
+    dialogueUI.style.display = "block";
   }
 }
