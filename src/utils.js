ae68c3ee878d51905080306fe5fc367962522173
DDeShon <36036284+DDeShon@users.noreply.github.com>
2024-11-22 21:46:04 -0600

    working on utils.js

--- src/utils.js
+++ src/utils.js
@@ -25,5 +25,8 @@ export function displayDialogue(text, onDisplayEnd) {
   function onCloseBtnClick() {
     onDisplayEnd();
     dialogueUI.style.display = "none";
+    dialogue.innerHTML = "";
+    clearInterval(intervalRef);
+    closeBtn.removeEventListener("click", onCloseBtnClick);
   }
 }
