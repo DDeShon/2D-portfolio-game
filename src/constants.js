68bbdf6a46fb7befb544e6364942948dca377e2b
DDeShon <36036284+DDeShon@users.noreply.github.com>
2024-12-11 12:07:26 -0600

    working on constants.js

--- src/constants.js
+++ src/constants.js
@@ -14,5 +14,5 @@ export const dialogueData = {
   projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
   Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas.`,
   library: `There are a lot of programming books on my shelves. I probably only read one of them. Who else compulsively buys technical books without ever finishing them?`,
-  exit: `If you want to exit Improbus' portfolio, just close the tab.`,
+  exit: `If you want to exit my portfolio, just close the tab.`,
 };
